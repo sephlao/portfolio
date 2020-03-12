275ac30f1be7f7a1c6db2ef1ff96797426256473
Joseph Lao <n01370825@nj-00118-017.humber.org>
2020-03-12 17:14:31 -0400

    added hero banner with greeting and tagline

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeroBanner from './HeroBanner'
+
+export default () => {
+  return (
+    <main>
+      <HeroBanner />
+    </main>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
