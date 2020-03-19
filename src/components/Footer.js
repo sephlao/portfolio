5f2550e77a10644105f2d5760ca4d1297dcf60b8
josephlaotzu <josephlao.1398@gmail.com>
2020-03-19 18:12:53 -0400

    added content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React from "react";
 
 export default () => {
-    return <footer>this is footer area</footer>
-  }
+  return (
+    <footer>
+      <div className="wrapper">
+      <ul className="site-nav">
+        <li>
+          <a href="#overview">Overview</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#education">Education</a>
+        </li>
+        <li>
+          <a href="#certificates">Certificates</a>
+        </li>
+      </ul>
+      <ul className="connect">
+        <li>
+          <a href="https://github.com/sephlao">@sephlao</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/josephvincentlao/">/in/josephvincentlao</a>
+        </li>
+        <li>
+          <a href="mailto:lao.josephv@gmail.com">lao.josephv@gmail.com</a>
+        </li>
+      </ul>
+      <p className="copyright">© 2020 Joseph Lao Personal Site</p>
+      </div>
+    </footer>
+  );
+};
